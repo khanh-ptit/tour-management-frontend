ec86e1bf646cf8cc3523c5df1ba540f951e829db
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-09-06 16:27:25 +0700

    fix/antd-spin-home

--- src/components/client/DestinationDomestic/index.js
+++ src/components/client/DestinationDomestic/index.js
@@ -19,87 +19,97 @@ function DestinationDomestic() {
   }, []);
 
   return (
-    <Spin spinning={loading} tip="Đang tải dữ liệu...">
-      <div className={`row g-3 ${styles["destination__list"]}`}>
-        {/* Ô lớn (chiếm 2 hàng, 1 cột) */}
-        {destinations.length > 0 && (
-          <div className="col-md-4">
-            <div
-              className={`${styles["destination__item"]} ${styles["large"]}`}
-            >
-              <div className={styles["destination__image"]}>
-                <img
-                  className={styles["destination__image-item"]}
-                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                  src={destinations[0].thumbnail}
-                  alt={destinations[0].name}
-                />
-                <div className={styles["destination__overlay"]}>
-                  <Link to={`destinations/${destinations[0].slug}`}>
-                    <button
-                      className={`button button__primary ${styles["destination__button"]}`}
-                    >
-                      Xem chi tiết
-                    </button>
-                  </Link>
+    <>
+      {destinations.length > 0 ? (
+        <div className={`row g-3 ${styles["destination__list"]}`}>
+          {/* Ô lớn (chiếm 2 hàng, 1 cột) */}
+          {destinations.length > 0 && (
+            <div className="col-md-4">
+              <div
+                className={`${styles["destination__item"]} ${styles["large"]}`}
+              >
+                <div className={styles["destination__image"]}>
+                  <img
+                    className={styles["destination__image-item"]}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={destinations[0].thumbnail}
+                    alt={destinations[0].name}
+                  />
+                  <div className={styles["destination__overlay"]}>
+                    <Link to={`destinations/${destinations[0].slug}`}>
+                      <button
+                        className={`button button__primary ${styles["destination__button"]}`}
+                      >
+                        Xem chi tiết
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-              </div>
 
-              <div className={styles["destination__content"]}>
-                <div className={styles["destination__name"]}>
-                  {destinations[0].name}
-                </div>
-                <div className={styles["destination__quantity"]}>
-                  {destinations[0].quantity} Tour
+                <div className={styles["destination__content"]}>
+                  <div className={styles["destination__name"]}>
+                    {destinations[0].name}
+                  </div>
+                  <div className={styles["destination__quantity"]}>
+                    {destinations[0].quantity} Tour
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {/* 4 ô nhỏ (2 hàng, 2 cột) */}
-        <div className="col-md-8">
-          <div className="row g-3">
-            {destinations.length > 1 &&
-              destinations.slice(1, 5).map((item) => (
-                <div key={item._id} className="col-md-6">
-                  <div className={styles["destination__item"]}>
-                    <div className={styles["destination__image"]}>
-                      <img
-                        className={styles["destination__image-item"]}
-                        src={item.thumbnail}
-                        alt={item.name}
-                        style={{
-                          width: "100%",
-                          height: "100%",
-                          objectFit: "cover",
-                        }}
-                      />
-                      <div className={styles["destination__overlay"]}>
-                        <Link to={`destinations/${item.slug}`}>
-                          <button
-                            className={`button button__primary ${styles["destination__button"]}`}
-                          >
-                            Xem chi tiết
-                          </button>
-                        </Link>
+          {/* 4 ô nhỏ (2 hàng, 2 cột) */}
+          <div className="col-md-8">
+            <div className="row g-3">
+              {destinations.length > 1 &&
+                destinations.slice(1, 5).map((item) => (
+                  <div key={item._id} className="col-md-6">
+                    <div className={styles["destination__item"]}>
+                      <div className={styles["destination__image"]}>
+                        <img
+                          className={styles["destination__image-item"]}
+                          src={item.thumbnail}
+                          alt={item.name}
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                          }}
+                        />
+                        <div className={styles["destination__overlay"]}>
+                          <Link to={`destinations/${item.slug}`}>
+                            <button
+                              className={`button button__primary ${styles["destination__button"]}`}
+                            >
+                              Xem chi tiết
+                            </button>
+                          </Link>
+                        </div>
                       </div>
-                    </div>
-                    <div className={styles["destination__content"]}>
-                      <div className={styles["destination__name"]}>
-                        {item.name}
-                      </div>
-                      <div className={styles["destination__quantity"]}>
-                        {item.quantity} Tour
+                      <div className={styles["destination__content"]}>
+                        <div className={styles["destination__name"]}>
+                          {item.name}
+                        </div>
+                        <div className={styles["destination__quantity"]}>
+                          {item.quantity} Tour
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+            </div>
           </div>
         </div>
-      </div>
-    </Spin>
+      ) : (
+        <Spin spinning={loading} tip="Đang tải dữ liệu...">
+          <div className={`row ${styles["box-waiting"]}`}></div>
+        </Spin>
+      )}
+    </>
   );
 }
 
